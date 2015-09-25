0abc3215ff01be66cbae4725b04b62a54f0c47e2
websideas <websideas.corp@gmail.com>
2015-09-25 23:12:21 +0700

    Add footer center one column

--- wp-content/themes/valorous/footer.php
+++ wp-content/themes/valorous/footer.php
@@ -48,7 +48,11 @@
                             <div class="container">
                                 <div class="row">
                                     <?php foreach($layouts as $i => $layout){ ?>
-                                        <div class="<?php echo esc_attr('col-md-'.$layout . ' col-sm-'.$layout.' col-xs-12'); ?>">
+                                        <?php
+
+                                            $footer_class = ($layout == 12) ? 'footer-area-one col-md-offset-2 col-md-8 col-sm-12 col-xs-12' : 'col-md-'.$layout . ' col-sm-'.$layout.' col-xs-12';
+                                        ?>
+                                        <div class="<?php echo esc_attr($footer_class); ?>">
                                             <?php dynamic_sidebar('footer-column-'.($i+1)) ?>
                                         </div>
                                     <?php } ?>
