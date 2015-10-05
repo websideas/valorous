715ccbbebb3f324f61512ba0e4d467e0ccd3cfae
tienthangtn <tienthangk43@gmail.com>
2015-10-05 08:46:06 +0700

    list update

--- wp-content/themes/valorous/framework/js_composer/vc_addons/list.php
+++ wp-content/themes/valorous/framework/js_composer/vc_addons/list.php
@@ -39,12 +39,11 @@ class WPBakeryShortCode_List extends WPBakeryShortCodesContainer {
 			'shortcode_custom' => vc_shortcode_custom_css_class( $css, ' ' )
 		);
 
-		$elementClass = preg_replace( array( '/\s+/', '/^\s|\s$/' ), array( ' ', '' ), implode( ' ', $elementClass ) );
-
 		if($css_animation !=''){
 			$data_animate = 'data-timeeffect="20" data-animation="'.$css_animation.'"';
 			$elementClass['animation'] = ' animation-effect';
 		}
+        $elementClass = preg_replace( array( '/\s+/', '/^\s|\s$/' ), array( ' ', '' ), implode( ' ', $elementClass ) );
 
 		return '<ul '.$data_animate.' class="'.esc_attr( $elementClass ).'">'. do_shortcode($content) . '</ul>';
 
@@ -200,23 +199,6 @@ vc_map( array(
             'value' => '',
             'description' => __( 'Animation.', 'js_composer' ),
         ),
-
-		array(
-			'type' => 'dropdown',
-			'heading' => __( 'CSS Animation', 'js_composer' ),
-			'param_name' => 'css_animation',
-			'admin_label' => true,
-			'value' => array(
-				__( 'No', 'js_composer' ) => '',
-				__( 'Top to bottom', 'js_composer' ) => 'top-to-bottom',
-				__( 'Bottom to top', 'js_composer' ) => 'bottom-to-top',
-				__( 'Left to right', 'js_composer' ) => 'left-to-right',
-				__( 'Right to left', 'js_composer' ) => 'right-to-left',
-				__( 'Appear from center', 'js_composer' ) => "appear"
-			),
-			'description' => __( 'Select type of animation if you want this element to be animated when it enters into the browsers viewport. Note: Works only in modern browsers.', 'js_composer' )
-		),
-
 
 
 
